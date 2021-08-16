b1089e6baa147f4d6fb85a03cf63b48f49412cd2
Carol Mendes <mendescarol920@gmail.com>
2021-08-16 11:25:17 -0300

    atualizado

---
+++ src/GlobalStyles.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle, createGlobalStyleComponent } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+:root{
+    --primary: #Fe0000;
+    --textPrimary: #000000;
+}
+*{
+
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+
+}
+ body{
+     font-family: sans-serif;
+     color: var(--textPrimary);
+
+ }
+
+`;
